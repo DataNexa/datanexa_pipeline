760708c15aad7e56aceacfd97401aae62d54be1f
Andrei Coelho <andreifcoelho@gmail.com>
2025-03-17 11:48:07 -0300

    inicio da versão 1.0

--- libs/log.py
+++
@@ -1,7 +0,0 @@
-
-
-def spider_log():
-    pass
-
-def api_log():
-    pass
--- test.py
+++
@@ -1,6 +0,0 @@
-from libs.request import get,post
-
-
-req = post('/monitoramento/fila_list', { "client_id":1 })
-
-print(req.body)
